8650ef07ea36ec1448956bdd06eb57d696509b42
Sergey <loveykosergey@ukr.net>
2023-10-06 19:10:16 +0300

    init: new project

--- iphone-14-pro/js/script.js
+++
@@ -1,6 +0,0 @@
-const burgerBtn = document.querySelector('.nav__btn'),
-      nav = document.querySelector('.nav');
-
-burgerBtn.addEventListener('click', ()=> {
-  nav.classList.toggle('menu__open');
-});
